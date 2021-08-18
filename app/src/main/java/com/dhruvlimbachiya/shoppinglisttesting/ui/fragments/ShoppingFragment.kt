7f6c4bb8b395094b2d4fa0126bf877ac97b32086
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-08-18 19:13:05 +0530

    Integrate ViewModel to all fragments.

--- app/src/main/java/com/dhruvlimbachiya/shoppinglisttesting/ui/fragments/ShoppingFragment.kt
+++ app/src/main/java/com/dhruvlimbachiya/shoppinglisttesting/ui/fragments/ShoppingFragment.kt
@@ -1,11 +1,23 @@
 package com.dhruvlimbachiya.shoppinglisttesting.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.dhruvlimbachiya.shoppinglisttesting.R
+import com.dhruvlimbachiya.shoppinglisttesting.ui.ShoppingViewModel
 
 /**
  * Created by Dhruv Limbachiya on 18-08-2021.
  */
 
 class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
+
+    private lateinit var mViewModel: ShoppingViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mViewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+    }
+
 }
