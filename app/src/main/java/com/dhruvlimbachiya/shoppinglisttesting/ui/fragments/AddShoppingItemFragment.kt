417f3fd621b8d745b7243df4c014298b23fb11c8
Dhruv Limbachiya <dhruv.mail4work@gmail.com>
2021-08-20 11:01:06 +0530

    nav : added navigation flow between fragments.

--- app/src/main/java/com/dhruvlimbachiya/shoppinglisttesting/ui/fragments/AddShoppingItemFragment.kt
+++ app/src/main/java/com/dhruvlimbachiya/shoppinglisttesting/ui/fragments/AddShoppingItemFragment.kt
@@ -2,10 +2,13 @@ package com.dhruvlimbachiya.shoppinglisttesting.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.dhruvlimbachiya.shoppinglisttesting.R
 import com.dhruvlimbachiya.shoppinglisttesting.ui.ShoppingViewModel
+import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
 
 /**
  * Created by Dhruv Limbachiya on 18-08-2021.
@@ -18,6 +21,25 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mViewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+
+
+        // Navigate to ImagePickFragment on ShoppingImage ImageView click.
+        ivShoppingImage.setOnClickListener {
+            findNavController().navigate(
+                AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
+            )
+        }
+
+        // Override the back press callback to implement image reset logic onBackPressed.
+        val backPressCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                mViewModel.setCurrentImageUrl("") // Reset the image url on back press.
+                findNavController().popBackStack()  // Analogous to when the user presses the system Back button.
+            }
+        }
+
+        // Add BackPressCallback.
+        requireActivity().onBackPressedDispatcher.addCallback(backPressCallback)
     }
 
 }
--- app/src/main/java/com/dhruvlimbachiya/shoppinglisttesting/ui/fragments/ShoppingFragment.kt
+++ app/src/main/java/com/dhruvlimbachiya/shoppinglisttesting/ui/fragments/ShoppingFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.dhruvlimbachiya.shoppinglisttesting.R
 import com.dhruvlimbachiya.shoppinglisttesting.ui.ShoppingViewModel
+import kotlinx.android.synthetic.main.fragment_shopping.*
 
 /**
  * Created by Dhruv Limbachiya on 18-08-2021.
@@ -18,6 +20,14 @@ class ShoppingFragment : Fragment(R.layout.fragment_shopping) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mViewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+
+
+        // Navigate to AddShoppingItemFragment on Fab Button click.
+        fabAddShoppingItem.setOnClickListener {
+            findNavController().navigate(
+                ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
+            )
+        }
     }
 
 }
